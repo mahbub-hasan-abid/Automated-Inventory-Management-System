59fd867c77e8ec404d3376fa599935a38bd09104
Tanvir Islam Robin <tanvirrobin18@cse.pstu.ac.bd>
2024-02-13 19:24:21 +0600

    Update main_home_screen.dart

--- lib/screens/main_home_screen.dart
+++ lib/screens/main_home_screen.dart
@@ -21,18 +21,72 @@ class _HomeScreenState extends State<HomeScreen> {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            customAppbar(),
-            SizedBox(
+            const customAppbar(),
+            const SizedBox(
               height: 50,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                cardMain(name: 'Product', number: 974),
-                cardMain(name: 'Customer', number: 1274),
-                cardMain(name: 'Categories', number: 37),
+                StreamBuilder(
+                    stream: FirebaseFirestore.instance
+                        .collection('products')
+                        .snapshots(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      final response = snapshot.data!.docs;
+
+                      return cardMain(name: 'Product', number: response.length);
+                    }),
+                StreamBuilder(
+                    stream: FirebaseFirestore.instance
+                        .collection('customers')
+                        .snapshots(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      final response = snapshot.data!.docs;
+
+                      return cardMain(
+                          name: 'Customer', number: response.length);
+                    }),
+                StreamBuilder(
+                    stream: FirebaseFirestore.instance
+                        .collection('categories')
+                        .snapshots(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      final response = snapshot.data!.docs;
+
+                      return cardMain(
+                          name: 'Categories', number: response.length);
+                    }),
                 cardMain(name: 'User', number: 57),
-                cardMain(name: 'Order', number: 74),
+                StreamBuilder(
+                    stream: FirebaseFirestore.instance
+                        .collection('orders')
+                        .snapshots(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      final response = snapshot.data!.docs;
+
+                      return cardMain(name: 'Orders', number: response.length);
+                    }),
               ],
             ),
             const SizedBox(
@@ -44,11 +98,11 @@ class _HomeScreenState extends State<HomeScreen> {
                     borderRadius: BorderRadius.all(Radius.circular(30))),
                 height: 400,
                 child: BarChartSample1(
-                  product_value: 74,
-                  customer_value: 80,
+                  product_value: 974,
+                  customer_value: 1274,
                   categories_value: 37,
                   user_value: 57,
-                  order_value: 69,
+                  order_value: 74,
                 ))
           ],
         ),
